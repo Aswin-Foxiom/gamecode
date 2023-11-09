20909db24d71ee2f8288d8bbe9cd8a85890a7b9d
Aswin <aswinfoxiom@gmail.com>
2023-11-09 12:41:36 +0530

    folders added

---
+++ src/pages/ProfilesPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProfilesPage() {
+  return <div>ProfilesPage</div>;
+}
+
+export default ProfilesPage;
---
+++ src/services/ApiCall.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ApiCall() {
+  return <div>ApiCall</div>;
+}
+
+export default ApiCall;
